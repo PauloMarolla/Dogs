4b39aaf3cfba5313e7b453639dca4e7cef74c1f1
Paulo Marolla <paulo.ricardo.marolla@hotmail.com>
2020-12-10 20:39:11 -0300

    UseFetch, UseForm

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -5,6 +5,11 @@ const types = {
     regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      'Obrigatório 1 letra Maiúscula, 1 Minúscula, 1 Dígito. Com no mínimo 8 caracteres',
+  },
 };
 
 const useForm = (type) => {
