aabb1fe19a4d0f3db1733b6b031bef898b5af34b
Paulo Marolla <paulo.ricardo.marolla@hotmail.com>
2020-12-16 19:50:36 -0300

    ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import User from './Components/User/User';
 import { UserStorage } from './UserContext';
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} end />
             <Route path="/login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
